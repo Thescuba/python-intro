f437d2aa7ca4db4dc451190ca89f66def2eba32d
Steve Tang <stevetang92@gmail.com>
2019-03-14 23:26:41 -0700

    add scripts folder

---
+++ scripts/error_demo.py
@@ -0,0 +1,15 @@
+# # example of syntax error
+# msg = 'Welcome to this program!
+# print(msg)
+
+# # fixed syntax error of code above
+# msg = 'Welcome to this program!'
+# print(msg)
+
+# # example of ValueError exception when inputing 'ten'
+# x = int(input('Enter a number: '))
+# x += 20
+# print(x)
+
+# # example of NameError exception
+# print(nonexistent_variable)
---
+++ scripts/input_demo.py
@@ -0,0 +1,5 @@
+# name = input("Enter your name: ")
+# print("Hello there, {}!".format(name.title()))
+
+name = input("Enter your name: ")
+print(5+name)
